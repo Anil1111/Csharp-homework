155ec44ac1f9231f79ee87acb08280872aceb1b3
June Lee <924458088@qq.com>
2018-09-12 01:05:56 +0800

    FirstHomeWork

---
+++ program1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dNum1 = 0;
+            double dNum2 = 0;
+            string s = "";
+            Console.WriteLine("Please input a number:");
+            s = Console.ReadLine();
+            dNum1 = double.Parse(s);
+            Console.WriteLine("Please input a number again:");
+            s = Console.ReadLine();
+            dNum2 = double.Parse(s);
+            Console.WriteLine("The result of the multiplication of these two numbers is:" + (dNum1 * dNum2));
+        }
+    }
+}
---
+++ program2/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace program2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double dNum1 = 0;
+            double dNum2 = 0;
+            string s = "";
+            double dRes = 0;
+            s = textBox1.Text;
+            dNum1 = double.Parse(s);
+            s = textBox2.Text;
+            dNum2 = double.Parse(s);
+            dRes = dNum1 * dNum2;
+            textBox3.Text = dRes.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
