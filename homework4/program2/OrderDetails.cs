b37f69e2acbc57408a5b8e6d7af6f6358c7b76b3
June Lee <924458088@qq.com>
2018-10-08 18:17:06 +0800

    homework4

---
+++ homework4/program2/OrderDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace program2
+{
+    class OrderDetails
+    {
+        public string GoodsName { get; set; }
+        public int GoodsPrice { get; set; }
+
+        public OrderDetails(string goodsName, int goodsPrice)
+        {
+            GoodsName = goodsName;
+            GoodsPrice = goodsPrice;
+        }
+    }
+}
